5d8d4e9298a5bb1c5c2b42003febaca4ff12794c
phanvuong <phanvanvuong1796@gmail.com>
2016-11-05 23:17:26 +0700

    update LastTime JsonFile

---
+++ app/src/main/java/com/vn/weather/entity/Clouds.java
@@ -0,0 +1,26 @@
+
+package com.vn.weather.entity;
+
+public class Clouds {
+
+    private int all;
+
+    /**
+     * 
+     * @return
+     *     The all
+     */
+    public int getAll() {
+        return all;
+    }
+
+    /**
+     * 
+     * @param all
+     *     The all
+     */
+    public void setAll(int all) {
+        this.all = all;
+    }
+
+}
---
+++ app/src/main/java/com/vn/weather/entity/Sys.java
@@ -0,0 +1,84 @@
+
+package com.vn.weather.entity;
+
+
+public class Sys {
+
+    private double message;
+    private String country;
+    private int sunrise;
+    private int sunset;
+
+    /**
+     * 
+     * @return
+     *     The message
+     */
+    public double getMessage() {
+        return message;
+    }
+
+    /**
+     * 
+     * @param message
+     *     The message
+     */
+    public void setMessage(double message) {
+        this.message = message;
+    }
+
+    /**
+     * 
+     * @return
+     *     The country
+     */
+    public String getCountry() {
+        return country;
+    }
+
+    /**
+     * 
+     * @param country
+     *     The country
+     */
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    /**
+     * 
+     * @return
+     *     The sunrise
+     */
+    public int getSunrise() {
+        return sunrise;
+    }
+
+    /**
+     * 
+     * @param sunrise
+     *     The sunrise
+     */
+    public void setSunrise(int sunrise) {
+        this.sunrise = sunrise;
+    }
+
+    /**
+     * 
+     * @return
+     *     The sunset
+     */
+    public int getSunset() {
+        return sunset;
+    }
+
+    /**
+     * 
+     * @param sunset
+     *     The sunset
+     */
+    public void setSunset(int sunset) {
+        this.sunset = sunset;
+    }
+
+}
