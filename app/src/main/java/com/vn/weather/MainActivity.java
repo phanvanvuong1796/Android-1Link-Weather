db7b844746f4fde97e576ca939644ce54c3bec2e
phanvuong <phanvanvuong1796@gmail.com>
2016-11-05 22:56:29 +0700

    update LastTime JsonFile

--- app/src/main/java/com/vn/weather/MainActivity.java
+++ app/src/main/java/com/vn/weather/MainActivity.java
@@ -1,15 +1,26 @@
 package com.vn.weather;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.vn.weather.asyncTask.CurrentWeatherAsyncTask;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
-    CurrentWeatherAsyncTask mCurrentWeather;
-    TextView txtJson;
+    private CurrentWeatherAsyncTask mCurrentWeather;
+    private TextView txtJson;
+    private TextView txtLast;
+    private File fileWriter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +28,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         txtJson = (TextView) findViewById(R.id.txt_json);
+        txtLast = (TextView) findViewById(R.id.txt_last);
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+
+        fileWriter = new File(path+"/Download/current_weather.json");
+        Log.e("PATH", path);
         mCurrentWeather = new CurrentWeatherAsyncTask(this);
         mCurrentWeather.execute();
         mCurrentWeather.setCallBack(new CurrentWeatherAsyncTask.CallBack() {
             @Override
             public void onFinish(String body) {
                 txtJson.setText(body);
+
+                try {
+                    FileWriter fileWriter1 = new FileWriter(fileWriter);
+                    fileWriter1.write(body);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                txtLast.setText(new SimpleDateFormat("dd/MM/yyyy HH:mm").format(fileWriter.lastModified()));
             }
         });
 
