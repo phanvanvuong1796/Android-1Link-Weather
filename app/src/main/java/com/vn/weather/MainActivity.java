320230da34d8413f1890c5b54f464ad498850581
phanvuong <phanvanvuong1796@gmail.com>
2016-11-05 22:23:22 +0700

    first commit

--- app/src/main/java/com/vn/weather/MainActivity.java
+++ app/src/main/java/com/vn/weather/MainActivity.java
@@ -2,12 +2,29 @@ package com.vn.weather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.vn.weather.asyncTask.CurrentWeatherAsyncTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    CurrentWeatherAsyncTask mCurrentWeather;
+    TextView txtJson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtJson = (TextView) findViewById(R.id.txt_json);
+        mCurrentWeather = new CurrentWeatherAsyncTask(this);
+        mCurrentWeather.execute();
+        mCurrentWeather.setCallBack(new CurrentWeatherAsyncTask.CallBack() {
+            @Override
+            public void onFinish(String body) {
+                txtJson.setText(body);
+            }
+        });
+
     }
 }
